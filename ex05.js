40d41e0bfa95835b35286dee0f77c230962fd99b
Nínive <ninive_cararo@studante.sesisenai.org.br>
2026-03-24 11:56:35 -0300

    add - fiz o ex 05

---
+++ ex05.js
@@ -0,0 +1,13 @@
+function transformacao() {
+    let numero = parseInt(prompt("Digite um número:"));
+
+    if (numero % 2 === 0) {
+        numero = numero + 1;
+    } else {
+        numero = numero + 1;
+    }
+
+    console.log("Resultado final:", numero);
+
+
+}
