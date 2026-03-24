6683400790b854fad3dea89814cb8738a5b51d6f
Nínive <ninive_cararo@studante.sesisenai.org.br>
2026-03-24 12:08:24 -0300

    add - fiz o ex 06

---
+++ ex06.js
@@ -0,0 +1,17 @@
+function verificador() {
+
+    let letra = prompt("Digite uma letra:");
+
+    if (
+        letra === "a" |
+        letra === "e" |
+        letra === "i" |
+        letra === "o" |
+        letra === "u"
+    ) {
+        console.log("É uma vogal");
+    } else {
+        console.log("É uma consoante");
+    }
+
+}
