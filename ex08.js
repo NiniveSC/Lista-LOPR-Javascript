6dcb7bd60bb49420542d61c1014a4816181ac036
Nínive <ninive_cararo@studante.sesisenai.org.br>
2026-03-26 12:40:57 -0300

    add - fiz o ex08

---
+++ ex08.js
@@ -0,0 +1,11 @@
+function operacaovariaveis() {
+    let x = 13;
+    let y = 6;
+
+    console.log("Diferença:", x - y);
+
+    console.log("Resultado do dobro do primeiro número + triplo da segunda:", (2 * x) + (3 * y));
+
+    console.log("Multiplicação:", x * y);
+
+}
