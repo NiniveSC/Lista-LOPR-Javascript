0c7bd6f34dfb7d1b7822f9eeeb0bd41d5b58afb3
Nínive <ninive_cararo@studante.sesisenai.org.br>
2026-03-31 09:14:57 -0300

    add - fiz o ex09

---
+++ ex09.js
@@ -0,0 +1,12 @@
+function ordenacaodoisnum() {
+    let x = 10
+    let y = 5
+
+    if (x > y) {
+        console.log("Do maior pro menor:", x, y)
+    } else {
+        console.log("Do maior pro menor:", y, x)
+    }
+
+
+}
